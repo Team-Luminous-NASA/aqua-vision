0ae72287705f5bef094df252eeb858fa2b45f3ae
Jubyer Ahmed Shezan <84038649+jashezan@users.noreply.github.com>
2024-10-04 14:18:03 +0600

    Updated Pace main page Component

---
+++ src/app/(main)/(nav)/pace/page.tsx
@@ -0,0 +1,11 @@
+import PageTitle from "@/components/common/page_title/PageTitle"
+
+const PaceMainPage = () => {
+  return (
+    <div>
+      <PageTitle pageTitle="PACE" />
+    </div>
+  );
+};
+
+export default PaceMainPage;
